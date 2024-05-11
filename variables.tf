3749031428f2412a90fcb4c789a1a0ed3b4ce99e
Hugo Souza <hugo.souza@ifood.com.br>
2024-05-11 17:31:01 -0300

    fix

--- variables.tf
+++ variables.tf
@@ -3,30 +3,30 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "CLIENT_ID" {
-  type    = string
-}
+# variable "CLIENT_ID" {
+#   type    = string
+# }
 
-variable "DB_URL" {
-  type    = string
-}
+# variable "DB_URL" {
+#   type    = string
+# }
 
-variable "PERM_PASS" {
-  type    = string
-}
+# variable "PERM_PASS" {
+#   type    = string
+# }
 
-variable "REGION" {
-  type    = string
-}
+# variable "REGION" {
+#   type    = string
+# }
 
-variable "USER_POOL_ID" {
-  type    = string
-}
+# variable "USER_POOL_ID" {
+#   type    = string
+# }
 
-variable "AWS_ACCESS_KEY" {
-  type    = string
-}
+# variable "AWS_ACCESS_KEY" {
+#   type    = string
+# }
 
-variable "AWS_SECRET_KEY" {
-  type    = string
-}
+# variable "AWS_SECRET_KEY" {
+#   type    = string
+# }
